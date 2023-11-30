b19e96e01aeddb0d449f93e5a88531271df835f1
tushar-454 <imtushar454@gmail.com>
2023-11-30 18:53:17 +0600

    frontend search funcition added

--- src/Hook/useApprovedArticles.jsx
+++ src/Hook/useApprovedArticles.jsx
@@ -1,20 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
 import useAxios from './useAxios';
 
-const useApprovedArticles = () => {
+const useApprovedArticles = (title = '', publication = '', tags = '') => {
   const axios = useAxios();
   const {
     data: approvedArticles,
     isLoading,
     isError,
+    refetch,
   } = useQuery({
     queryKey: ['approvedArticles'],
     queryFn: async () => {
-      const res = await axios.get(`/articles?isApprove=Approved`);
+      const res = await axios.get(
+        `/articles?isApprove=Approved&title=${title}&publication=${publication}&tags=${tags}`
+      );
       return res.data;
     },
   });
-  return { approvedArticles, isLoading, isError };
+  return { approvedArticles, isLoading, isError, refetch };
 };
 
 export default useApprovedArticles;
--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import toast from 'react-hot-toast';
 import { FaRegClock } from 'react-icons/fa6';
@@ -14,9 +15,52 @@ import SectionTitle from '../Shared/SectionTitle';
 import premium from '../assets/icon/premium.png';
 
 const Articles = () => {
-  const { approvedArticles, isLoading, isError } = useApprovedArticles();
+  const [title, setTitle] = useState('');
+  const [tags, setTags] = useState('');
+  const [publication, setPublication] = useState('');
+  const { approvedArticles, isLoading, isError, refetch } = useApprovedArticles(
+    title,
+    publication,
+    tags
+  );
   const { publications, isLoading: publicationLoad } = usePublications();
   const { userInfo, isLoading: userLoad } = useUserInfo();
+  // handle title search
+  const handleTitleSearch = (e) => {
+    e.preventDefault();
+    setTags('');
+    setPublication('');
+    setTitle(e.target.title.value);
+    setTimeout(() => {
+      refetch();
+    }, 0);
+  };
+  const handlePublicationSearch = (e) => {
+    setTitle('');
+    setTags('');
+    setPublication(e.target.value);
+    setTimeout(() => {
+      refetch();
+    }, 0);
+  };
+  const handleTagSearch = (e) => {
+    e.preventDefault();
+    setTitle('');
+    setPublication('');
+    setTags(e.target.tags.value);
+    setTimeout(() => {
+      refetch();
+    }, 0);
+  };
+  const resetSearch = (e) => {
+    if (e.target.value.length === 0) {
+      setTitle('');
+      setTags('');
+    }
+    setTimeout(() => {
+      refetch();
+    }, 0);
+  };
   return (
     <section>
       <Helmet>
@@ -25,25 +69,30 @@ const Articles = () => {
       <Container>
         <SectionTitle>All Articles</SectionTitle>
         {/* search divition  */}
-        <div className='my-10 flex gap-5'>
+        <div className='my-10 flex flex-col sm:flex-row gap-5'>
           <div className='flex flex-col items-center'>
-            <form>
+            <form onSubmit={handleTitleSearch}>
               <Input
                 displayName={'Search By Title'}
                 placeholder='enter search title'
                 id={'title'}
                 name={'title'}
+                onChange={resetSearch}
               />
               <Button type='submit' displayName={'Search'} />
             </form>
           </div>
           <div className='flex flex-col items-center'>
-            <Input
-              displayName={'Search By Tag'}
-              placeholder='enter search tags'
-              id={'title'}
-            />
-            <Button displayName={'Search'} />
+            <form onSubmit={handleTagSearch}>
+              <Input
+                displayName={'Search By Tag'}
+                placeholder='enter search tags'
+                id={'tags'}
+                name={'tags'}
+                onChange={resetSearch}
+              />
+              <Button displayName={'Search'} type={'submit'} />
+            </form>
           </div>
           <div>
             <div>
@@ -57,6 +106,7 @@ const Articles = () => {
                 name='publication'
                 id='publication'
                 className='border p-4 rounded-lg outline-none w-full text-lg'
+                onChange={handlePublicationSearch}
               >
                 <option value=''>Choose your publication</option>
                 {isError && (
@@ -73,68 +123,69 @@ const Articles = () => {
           </div>
         </div>
         {/* all article div grid  */}
-        <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-10'>
-          {isError && toast.error('There was an error')}
-          {!isLoading &&
-            approvedArticles.length === 0 &&
-            toast.error('No articles found')}
-          {!isLoading &&
-            approvedArticles?.map((article, index) => (
-              <div
-                key={index}
-                className={`${
-                  article.isApprove !== 'Approved' ? 'hidden' : 'block'
-                } p-5 border rounded-lg bg-[${
-                  color[Math.floor(Math.random() * color.length)]
-                }] relative`}
-              >
-                {article.isPremium === 'Approved' && (
-                  <span className='absolute -top-5 -right-5'>
-                    <img src={premium} className='w-20' />
-                  </span>
-                )}
-                <div>
-                  <img
-                    src={article.image}
-                    className='w-full h-48 object-cover rounded-lg'
-                  />
-                </div>
-                <div className='my-3 flex flex-wrap gap-2'>
-                  {article.tags.map((tag, idx) => (
-                    <span
-                      key={idx}
-                      className='text-sm px-1 rounded-full bg-slate-50'
-                    >
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-                <h1 className='text-xl'>{article.title}</h1>
-                <div className='flex justify-between items-center'>
-                  <h1 className='text-sm flex items-center gap-2 mt-4'>
-                    <FaRegClock />
-                    {new Date(article.date).toLocaleString()}
-                  </h1>
-                  <h1 className='text-sm flex items-center gap-2 mt-4'>
-                    <IoEyeOutline className='text-xl' />
-                    {article.viewCount}
-                  </h1>
-                </div>
-                <h2 className='my-4 font-bold'>
-                  Publication: {article.publication}
-                </h2>
-                <p>{article.description.slice(0, 111)}</p>
-                <Link
-                  to={
-                    article.isPremium === 'Approved' &&
-                    !userLoad &&
-                    !userInfo.isPremium
-                      ? ''
-                      : `articles/${article._id}`
-                  }
+        {isError && toast.error('There was an error')}
+        {!isLoading &&
+          approvedArticles.length === 0 &&
+          toast.error('No articles found')}
+        {!isLoading && approvedArticles.length > 0 && (
+          <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-10'>
+            {!isLoading &&
+              approvedArticles?.map((article, index) => (
+                <div
+                  key={index}
+                  className={`${
+                    article.isApprove !== 'Approved' ? 'hidden' : 'block'
+                  } p-5 border rounded-lg bg-[${
+                    color[Math.floor(Math.random() * color.length)]
+                  }] relative`}
                 >
-                  <button
-                    className={`inline-block mt-6 bg-pink-400 text-white
+                  {article.isPremium === 'Approved' && (
+                    <span className='absolute -top-5 -right-5'>
+                      <img src={premium} className='w-20' />
+                    </span>
+                  )}
+                  <div>
+                    <img
+                      src={article.image}
+                      className='w-full h-48 object-cover rounded-lg'
+                    />
+                  </div>
+                  <div className='my-3 flex flex-wrap gap-2'>
+                    {article.tags.map((tag, idx) => (
+                      <span
+                        key={idx}
+                        className='text-sm px-1 rounded-full bg-slate-50'
+                      >
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
+                  <h1 className='text-xl'>{article.title}</h1>
+                  <div className='flex justify-between items-center'>
+                    <h1 className='text-sm flex items-center gap-2 mt-4'>
+                      <FaRegClock />
+                      {new Date(article.date).toLocaleString()}
+                    </h1>
+                    <h1 className='text-sm flex items-center gap-2 mt-4'>
+                      <IoEyeOutline className='text-xl' />
+                      {article.viewCount}
+                    </h1>
+                  </div>
+                  <h2 className='my-4 font-bold'>
+                    Publication: {article.publication}
+                  </h2>
+                  <p>{article.description.slice(0, 111)}</p>
+                  <Link
+                    to={
+                      article.isPremium === 'Approved' &&
+                      !userLoad &&
+                      !userInfo.isPremium
+                        ? ''
+                        : `articles/${article._id}`
+                    }
+                  >
+                    <button
+                      className={`inline-block mt-6 bg-pink-400 text-white
                     font-medium p-3 rounded-lg ${
                       article.isPremium === 'Approved' &&
                       !userLoad &&
@@ -142,13 +193,14 @@ const Articles = () => {
                         ? 'cursor-not-allowed bg-pink-200'
                         : 'cursor-pointer'
                     }`}
-                  >
-                    View Details
-                  </button>
-                </Link>
-              </div>
-            ))}
-        </div>
+                    >
+                      View Details
+                    </button>
+                  </Link>
+                </div>
+              ))}
+          </div>
+        )}
       </Container>
     </section>
   );
